32f3bc7592ae9d716a9c558314ac57d010de551c
cheaton622 <cheaton622@gmail.com>
2022-08-30 09:54:06 -0500

    Home

---
+++ Home.py
@@ -0,0 +1,19 @@
+from enum import auto
+from tkinter import PAGES
+import markdown
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import pandas as pd
+import numpy as np
+from streamlit_option_menu import option_menu
+
+
+st.set_page_config(page_title="Code Enforcement Issues",page_icon="🏚️",layout='wide',initial_sidebar_state='collapsed')
+
+
+
+
+st.title("Blah Blah")
+
+  
